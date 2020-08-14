fe21b8bb66f209b69a8aa672fd59eb99ae38a483
Purnima Sharma <57852378+purnima143@users.noreply.github.com>
2020-08-14 12:39:31 +0530

    deployed unity proj

---
+++ Assets/HelloAR/scripts/ARController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GoogleARCore;
+
+
+#if UNITY_EDITOR
+using input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class ARController : MonoBehaviour
+{
+    public GameObject GridPrefab;
+    public GameObject Portal;
+    public GameObject ARCamera;
+    private List<DetectedPlane> m_NewTrackedPlanes = new List<DetectedPlane>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+        Session.GetTrackables<DetectedPlane>(m_NewTrackedPlanes, TrackableQueryFilter.New);
+        for (int i = 0; i < m_NewTrackedPlanes.Count; i++)
+        {
+            GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
+            grid.GetComponent<GridVisualiser>().Initialize(m_NewTrackedPlanes[i]);
+        }
+
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+        TrackableHit hit;
+        if (Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon, out hit))
+        {
+            Portal.SetActive(true);
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            Portal.transform.position = hit.Pose.position;
+
+            Vector3 cameraPosition = ARCamera.transform.position;
+            cameraPosition.y = hit.Pose.position.y;
+
+            Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+            Portal.transform.parent = anchor.transform;
+        }
+    }
+}
---
+++ Assets/HelloAR/scripts/PortalManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Rendering;
+public class PortalManager : MonoBehaviour
+{
+    public GameObject MainCamera;
+    public GameObject Sponza;
+
+    private Material[] SponzaMaterials;
+    private Material PortalPlaneMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SponzaMaterials = Sponza.GetComponent<Renderer>().sharedMaterials;
+        PortalPlaneMaterial = GetComponent<Renderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider collider)
+    {
+        Vector3 camPositionInPortalSpace = transform.InverseTransformPoint(MainCamera.transform.position);
+
+        if (camPositionInPortalSpace.y <= 0.0f)
+        {
+            for (int i = 0; i < SponzaMaterials.Length; i++)
+            {
+                SponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            }
+            PortalPlaneMaterial.SetInt("_CullMode", (int)CullMode.Front);
+        }
+        else if (camPositionInPortalSpace.y < 0.5f)
+        {
+            for (int i = 0; i < SponzaMaterials.Length; i++)
+            {
+                SponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Always);
+            }
+            PortalPlaneMaterial.SetInt("_CullMode", (int)CullMode.Off);
+
+        }
+        else
+        {
+            for (int i = 0; i < SponzaMaterials.Length; i++)
+            {
+                SponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Equal);
+            }
+            PortalPlaneMaterial.SetInt("_CullMode", (int)CullMode.Back);
+
+        }
+    }
+}
